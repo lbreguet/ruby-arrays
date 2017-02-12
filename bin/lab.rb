7370e4b07cae14aec446d573cd1eda2f3c4de41a
lbreguet <louis-w@live.com>
2017-02-11 19:11:12 -0500

    Ruby arrays intro
    -review arrays

--- bin/lab.rb
+++ bin/lab.rb
@@ -3,20 +3,20 @@
 working_array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 # Assign `20` to the end of the array using the array's length.
-
+working_array[working_array.length] << 20
 # Access the 3rd element from the end of the array.
-
+working_array[-3]
 # Access element 9 from the array
-
+working_array[8]
 # Assign `[-12, -49]` to the 5th element from the end.
-
+working_array[-5] << [-12, -49]
 # Access all the elements starting at index 1.
-
+working_array[1, working_array.length]
 ## Bonus ##
 # In bin/lab.rb use a block initializer with Array.new to create
 # a Ruby Array with ten elements where elements are equal to their
 # index multiplied by 2. Store the result and display it on the
 # console with puts <array name>.
 
-# binding.pry
-# ""
+binding.pry
+""
---
+++ bin/story.rb
@@ -0,0 +1,7 @@
+character = %w{Luke Han Ben Chewie Leia C3-P0 R2-D2 Vader}
+
+puts 'A long time ago in a galaxy far, far away..'
+
+puts "#{character[0]} bought #{character[5]} and #{character[6]}"
+puts "They saved the galaxy with #{character[1]}, #{character[2]}, #{character[3]}, rescued Princess #{character[-3]},"
+puts "and defeated #{character[7]}"
